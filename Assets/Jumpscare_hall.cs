08ffe20f360116d5d6a36b58a171e1d73d028a41
giovax11 <giovax1110@gmail.com>
2023-12-20 22:21:50 -0400

    Screamer del pasillo y effectos en la musica

--- Assets/Jumpscare_hall.cs
+++ Assets/Jumpscare_hall.cs
@@ -2,16 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Jumpscare_hall : MonoBehaviour
 {
-   
-    public GameObject gramophone;
+    public GameObject monster;
+    public GameObject screamSource;
+    private AudioSource screamAudio;
     private bool hasTriggered = false;
+    private bool isMoving = false;
 
-    private void OnTriggerEnter(Collider other){
-         if (!hasTriggered && other.CompareTag("Player")){
-
-         }
+  private void Start()
+    {
+        screamAudio = screamSource.GetComponent<AudioSource>();
+        if (screamAudio == null)
+        {
+            Debug.LogError("El componente AudioSource no se ha encontrado en el objeto <link>gramophone</link>.");
+        }
     }
-   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasTriggered && other.CompareTag("Player"))
+        {
+           if (screamAudio != null)
+            {
+                screamAudio.Play();}
+
+            hasTriggered = true;
+            isMoving = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isMoving && monster!=null)
+        {
+            Vector3 movement = new Vector3(0, 0, 5.8f); 
+;
+            monster.transform.Translate(movement * 5 * Time.deltaTime);
+             Destroy(monster, 4f);
+        }
+    }
 }
