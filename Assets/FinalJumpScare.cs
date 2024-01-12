aa84bd6c2c8e4c0b594009ef532868c1f76ebd05
giovax11 <giovax1110@gmail.com>
2024-01-12 19:00:43 -0400

    jumscare final y trigger de activacion

--- Assets/FinalJumpScare.cs
+++ Assets/FinalJumpScare.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PSXShaderKit;
 
 public class FinalJumpScare : MonoBehaviour
 {
@@ -13,6 +14,26 @@ public class FinalJumpScare : MonoBehaviour
     public Animator animator;
 
     private AudioSource screamAudio;
+
+    public AudioSource endingAudio;
+
+    
+    private bool hasPlayedEndingAudio = false;
+
+
+    public float distanceThreshold = 1.5f; 
+
+    public GameObject monsterFinalJumpscareEnding;
+
+    public PosterizeEffect posterize;
+
+    public BadTVEffect tvEffect;
+
+    public PSXPostProcessEffect pixelation;
+
+
+    private float transitionDuration = 2f; 
+    private float elapsedTime = 0f;
 
 
     void Start()
@@ -49,8 +70,24 @@ public class FinalJumpScare : MonoBehaviour
         {
             Vector3 direccion = player.position - monster.transform.position; // Cambiado a monster.transform.position
             direccion.Normalize();
-            float velocidad = 15f;
+            float velocidad = 12f;
             monster.transform.position += direccion * velocidad * Time.deltaTime;
+            float distance = Vector3.Distance(monster.transform.position, player.position);
+              if (distance < distanceThreshold)
+            {
+                if(!hasPlayedEndingAudio){
+                endingAudio.Play();
+                hasPlayedEndingAudio=true;}
+                monster.SetActive(false);
+                monsterFinalJumpscareEnding.SetActive(true);
+                elapsedTime += Time.deltaTime;
+                float t = Mathf.Clamp01(elapsedTime / transitionDuration);
+                posterize.level = Mathf.RoundToInt(Mathf.Lerp(60, 2, t));
+                tvEffect.thickDistort = (float)Mathf.RoundToInt(Mathf.Lerp(0.3f, 10f, t));
+                tvEffect.fineDistort = Mathf.RoundToInt(Mathf.Lerp(2.2f, 10f, t));
+                pixelation._PixelationFactor=(Mathf.Lerp(0.3f, 0.17f, t));
+
+            }
         }
     }
 }
